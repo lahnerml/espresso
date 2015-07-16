84369149b75ec8a8e00b975f968eacda1c14a8b1
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2015-07-16 14:19:57 +0200

    erased trailing whitespaces

--- src/tcl/grid_tcl.cpp
+++ src/tcl/grid_tcl.cpp
@@ -1,27 +1,27 @@
 /*
   Copyright (C) 2010,2012,2013,2014 The ESPResSo project
-  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
+  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010
     Max-Planck-Institute for Polymer Research, Theory Group
-  
+
   This file is part of ESPResSo.
-  
+
   ESPResSo is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   ESPResSo is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** \file grid.cpp   Domain decomposition for parallel computing.
  *
- *  For more information on the domain decomposition, 
- *  see \ref grid.hpp "grid.h". 
+ *  For more information on the domain decomposition,
+ *  see \ref grid.hpp "grid.h".
 */
 #include "utils.hpp"
 #include "parser.hpp"
@@ -43,7 +43,7 @@ int tclcallback_node_grid(Tcl_Interp *interp, void *_data)
     return (TCL_ERROR);
   }
 
-  /* outsourced to 
+  /* outsourced to
      sort_int_array(data,3); */
 
   node_grid[0] = data[0];
@@ -73,7 +73,7 @@ int tclcallback_periodicity(Tcl_Interp *interp, void *_data)
   if ((tmp_periodic & 7) == 7)
     return (TCL_OK);
 
-  Tcl_AppendResult(interp, "periodic cannot be set since PARTIAL_PERIODIC not configured.", (char *)NULL);  
+  Tcl_AppendResult(interp, "periodic cannot be set since PARTIAL_PERIODIC not configured.", (char *)NULL);
   return (TCL_ERROR);
 }
 
@@ -100,14 +100,14 @@ int tclcallback_box_l(Tcl_Interp *interp, void *_data)
 int tclcommand_change_volume(ClientData data, Tcl_Interp *interp, int argc, char **argv) {
   char buffer[50 + TCL_DOUBLE_SPACE + TCL_INTEGER_SPACE];
   char *mode;
-  double d_new = box_l[0]; 
+  double d_new = box_l[0];
   int dir = -1;
 
   if (argc < 2) {
     Tcl_AppendResult(interp, "Wrong # of args! Usage: change_volume { <V_new> | <L_new> { x | y | z | xyz } }", (char *)NULL); return (TCL_ERROR);
   }
   if (Tcl_GetDouble(interp, argv[1], &d_new) == TCL_ERROR) return (TCL_ERROR);
-  if (argc == 3) { 
+  if (argc == 3) {
     mode = argv[2];
     if (!strncmp(mode, "x", strlen(mode))) dir = 0;
     else if (!strncmp(mode, "y", strlen(mode))) dir = 1;
@@ -122,8 +122,8 @@ int tclcommand_change_volume(ClientData data, Tcl_Interp *interp, int argc, char
     d_new = pow(d_new,1./3.);
     rescale_boxl(3,d_new);
   }
-  else { 
-    rescale_boxl(dir,d_new); 
+  else {
+    rescale_boxl(dir,d_new);
   }
   sprintf(buffer, "%f", box_l[0]*box_l[1]*box_l[2]);
   Tcl_AppendResult(interp, buffer, (char *)NULL);

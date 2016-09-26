128952ac84410781529d1a01146944c31c7c059d
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2016-09-26 16:19:46 +0900

    fix reg refinement. todo: mapping between trees
    mapping is needed for dynamic adaptivity

--- src/core/communication.cpp
+++ src/core/communication.cpp
@@ -2865,6 +2865,22 @@ void mpi_reg_refinement (int node, int param) {
                      NULL);                  // replace data
 
   p8est_partition (p8est, 0, NULL);
+  p8est_ghostvirt_destroy(lbadapt_ghost_virt);
+  p8est_mesh_destroy(lbadapt_mesh);
+  p8est_ghost_destroy(lbadapt_ghost);
+
+  lbadapt_ghost = p8est_ghost_new(p8est, P8EST_CONNECT_EDGE);
+  lbadapt_mesh = p8est_mesh_new_ext(p8est, lbadapt_ghost, 1, 1, 1, P8EST_CONNECT_EDGE);
+  lbadapt_ghost_virt = p8est_ghostvirt_new(p8est, lbadapt_ghost, lbadapt_mesh);
+  int old_flg = finest_level_global;
+  finest_level_global = lbadapt_get_global_maxlevel();
+  // due to non-recursive refinement only 2 cases can occur: finest level
+  // increases or not.
+  lb_step_factor = (old_flg == finest_level_global) ? lb_step_factor : 0.5 * lb_step_factor;
+
+  // FIXME: Implement mapping between two trees
+  lb_release_fluid();
+  lb_reinit_fluid();
 #endif // LB_ADAPTIVE
 }
 

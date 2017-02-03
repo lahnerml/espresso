5dcd63c00af15c93aeffba02d4f4e7a435ef2dbf
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2017-02-03 11:04:38 +0100

    dirt fix

--- src/tcl/lb-adaptive_tcl.cpp
+++ src/tcl/lb-adaptive_tcl.cpp
@@ -18,9 +18,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    */
-#include "lb-adaptive_tcl.hpp"
 #include "communication.hpp"
 #include "lb-boundaries.hpp"
+#include "lb-adaptive_tcl.hpp"
 
 #include <stdio.h>
 

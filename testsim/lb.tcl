15ae15fefde5dbe0d6ea3ad8c027f34ebef6429a
brunnme <maltebrunn@web.de>
2017-04-08 22:47:44 +0200

    change in testsim script

--- testsim/lb.tcl
+++ testsim/lb.tcl
@@ -1,7 +1,7 @@
 set base_level 1
 set max_level 2
 
-set final_time 400.0
+set final_time 1000.0
 set force 0.001
 
 set do_refine 1
@@ -25,16 +25,20 @@ setmd time_step $dt_real
 lbadapt-init $base_level
 lbadapt_set_max_level $max_level
 
-lbfluid cpu agrid $agrid dens 1 visc 0.05 tau $dt_real friction 0.5 ext_force $force 0 0
+lbfluid cpu agrid $agrid dens 1 visc 0.05 tau $dt_real friction 0.5 ext_force 0 0 0
 
 if {$with_wall >= 1} {
   lbboundary wall dist  0.5 normal 0  1 0 ;#velocity  0.1 0 0
   lbboundary wall dist -11.5 normal 0 -1 0 ;#velocity -0.1 0 0
+  
+  lbboundary rhomboid corner -0.5 5.5 -0.5 a 5 0 0 b 0 1 0 c 0 0 2 direction outside velocity 0.1 0 0
 }
 
 if {$do_refine >= 1} {
   #lbadapt-geom-ref
-  lbadapt-regref 0 4 4.5 7.5 0 1
+  #lbadapt-regref 0 4 4.5 7.5 0 1
+  lbadapt-regref 0 4 8 12 0 1
+  lbadapt-regref 0 4 0 4 0 1
 }
 
 lbfluid print vtk velocity lb_${base_level}_${max_level}_0.vtk
--- testsim/simple_lb_particle.tcl
+++ testsim/simple_lb_particle.tcl
@@ -1,8 +1,8 @@
 cellsystem domain_decomposition -no_verlet_list
 
-set n_part 5
+set n_part 0
 set iter 200
-set iteri 200
+set iteri 50
 
 setmd skin 0.1
 
@@ -12,66 +12,64 @@ set boxl 4.0
 
 setmd box_l $boxl $boxl 1
 
-part 0 pos [expr $boxl*0.5] 1.9 0.5 q 0.0 type 0 v 0 0 1 ext_force 0.1 0 0
-#part 0 pos 1.162820 2.642298 0.1 q 0.0 type 0 v 0 0 -1 ext_force 0 0 0
+#part 0 pos 1.9 1.9 0.5 q 0.0 type 0 v 0.1 0 0 ext_force 0.00 0 0 fix 1 1 1
+part 0 pos 2.0 1.9 0.5 q 0.0 type 0 v 0 0 0 ;#ext_force 0.01 0 0
 
+set base 3
 set mlvl 3
 set wall 0.5
 
 set agrid [expr 1./pow(2, $mlvl)]
 
 expr srand([pid])
-for {set i 1} {$i < $n_part} {incr i} {
+for {set i 1} {$i <= $n_part} {incr i} {
   set pos_x [expr rand()*($boxl-2*$wall) + $wall]
   set pos_y [expr rand()*($boxl-2*$wall) + $wall]
   set pos_z [expr rand()*1.0]
   part $i pos $pos_x $pos_y $pos_z q 0.0 type 0 ext_force 0.1 0 0
 }
 
-set dt 0.002
+set dt 0.01
+
+set prefac [expr pow(2, $mlvl - 1)]
 
 thermostat lb 0
-setmd time_step [expr $dt*0.5]
+setmd time_step [expr $dt/$prefac]
 
-puts "Mindist: [analyze mindist]"
-minimize_energy 10.0 100 0.1 0.01
-kill_particle_motion
-kill_particle_forces
-puts "Mindist: [analyze mindist]"
+#puts "Mindist: [analyze mindist]"
+#minimize_energy 10.0 100 0.1 0.01
+#kill_particle_motion
+#kill_particle_forces
+#puts "Mindist: [analyze mindist]"
 
-lbadapt-init 2
+lbadapt-init $base
 lbadapt_set_max_level $mlvl
 
-lbfluid cpu agrid $agrid dens 1. visc 0.032 tau [expr $dt*0.5] friction 0.5
-#ext_force 0.01 0.0 0.0
+lbfluid cpu agrid $agrid dens 1. visc 0.032 tau [expr $dt/$prefac] friction 0.5 ext_force 0.01 0.0 0.0
 
-# set inflow/outflow boundaries
-#lbboundary wall dist $agrid normal 1 0 0  velocity 1 0 0
-#lbboundary wall dist [expr -$boxl + $agrid] normal -1 0 0 velocity 1 0 0
-# setup a channel
-lbboundary wall dist $wall normal 0 1 0
-lbboundary wall dist [expr -$boxl + $wall] normal 0 -1 0
-#lbboundary wall dist 0.5 normal 0 1 0
-#lbboundary wall dist [expr $boxl -0.5] normal 0 -1 0
+lbboundary wall dist $wall normal 0 1 0 
+lbboundary wall dist [expr -$boxl + $wall] normal 0 -1 0 
 
-#lbadapt-exclude-bnd-from-geom-ref 0
-#lbadapt-exclude-bnd-from-geom-ref 1
+#lbadapt-geom-ref
 
-lbadapt-geom-ref
-
-lbfluid print vtk boundary boundary.vtk
+#lbfluid print vtk boundary boundary.vtk
 
 lbadapt-reset-fluid
 
+lbfluid print vtk velocity  lb_0.vtk
+md_vtk md_0.vtk
+
 for { set i 0 } { $i < $iter } { incr i } {
-  md_vtk "md_part_$i.vtk"
-  lbfluid print vtk velocity  "lb_field_$i.vtk"
-  integrate $iteri
+  #md_vtk "md_part_$i.vtk"
+  #lbfluid print vtk velocity  "lb_field_$i.vtk"
+  integrate [expr $iteri*int($prefac)]
   puts -nonewline "run $i at time=[setmd time]\r"
   flush stdout
 }
 
-lbfluid print vtk velocity  "lb_field_$iter.vtk"
-md_vtk "md_part_$iter.vtk"
+#lbfluid print vtk velocity  "lb_field_$iter.vtk"
+lbfluid print vtk velocity  lb_1.vtk
+#md_vtk "md_part_$iter.vtk"
+md_vtk md_1.vtk
 
 exit

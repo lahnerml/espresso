1f4e3441066db7d53da083a14eccabe6854e885e
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2017-06-28 11:05:00 +0200

    Use bmi2 for fun and profit

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -1,6 +1,10 @@
 #include <cstring>
 #include "utils.hpp"
 
+#ifdef __BMI2__
+#include <x86intrin.h>
+#endif
+
 char *strcat_alloc(char *left, const char *right) {
   if (!left) {
     char *res = (char *)Utils::malloc(strlen(right) + 1);
@@ -18,18 +22,20 @@ char *strcat_alloc(char *left, const char *right) {
 // Returns the morton index for given cartesian coordinates.
 // Note: This is not the index of the p4est quadrants. But the ordering is the same.
 int64_t dd_p4est_cell_morton_idx(int x, int y, int z) {
-  //p4est_quadrant_t c;
-  //c.x = x; c.y = y; c.z = z;
-  /*if (x < 0 || x >= grid_size[0])
-    runtimeErrorMsg() << x << "x" << y << "x" << z << " no valid cell";
-  if (y < 0 || y >= grid_size[1])
-    runtimeErrorMsg() << x << "x" << y << "x" << z << " no valid cell";
-  if (z < 0 || z >= grid_size[2])
-    runtimeErrorMsg() << x << "x" << y << "x" << z << " no valid cell";*/
-  
+#ifdef __BMI2__
+#warning "Using BMI2 for cell_morton_idx"
+  static const unsigned mask_x = 0x49249249;
+  static const unsigned mask_y = 0x92492492;
+  static const unsigned mask_z = 0x24924924;
+
+  return _pdep_u32(x, mask_x)
+           | _pdep_u32(y, mask_y)
+           | _pdep_u32(z, mask_z);
+#else
+#warning "BMI2 not detected: Using slow loop version for cell_morton_idx"
   int64_t idx = 0;
   int64_t pos = 1;
-  
+
   for (int i = 0; i < 21; ++i) {
     if ((x&1)) idx += pos;
     x >>= 1; pos <<= 1;
@@ -38,8 +44,7 @@ int64_t dd_p4est_cell_morton_idx(int x, int y, int z) {
     if ((z&1)) idx += pos;
     z >>= 1; pos <<= 1;
   }
-  
+
   return idx;
-  //c.level = P4EST_QMAXLEVEL;
-  //return p4est_quadrant_linear_id(&c,P4EST_QMAXLEVEL);
+#endif
 }

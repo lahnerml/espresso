19f331d56b0028d55a4764a7ae75b85fcabf6417
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2017-07-19 10:38:36 +0200

    Save additional resorting step

--- src/core/domain_decomposition.cpp
+++ src/core/domain_decomposition.cpp
@@ -1044,6 +1044,9 @@ void dd_topology_init(CellPList *old, bool isRepart) {
     dd_p4est_global_exchange_part(local_cells.cell[0]);
   else // Call it anyway in case there are no cells on this node (happens during startup)
     dd_p4est_global_exchange_part(NULL);
+
+  // We just resorted properly
+  resort_particles = 0;
 #else
   /* copy particles */
   for (c = 0; c < old->n; c++) {

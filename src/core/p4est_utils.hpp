8fa0a3060e7f476c811ee05ebfe331862b8d92c7
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2018-04-12 17:25:57 +0200

    Improve documentation

--- src/core/p4est_utils.hpp
+++ src/core/p4est_utils.hpp
@@ -261,7 +261,8 @@ p4est_locidx_t p4est_utils_idx_to_qid(forest_order forest, p4est_gloidx_t idx);
 
 /** Map a geometric position to the respective processor.
  *
- * @param forest   The forest_info containing the p4est for which to map.
+ * @param forest   The position of the forest info containing the p4est for
+ *                 which to map.
  * @param xyz      The position to map.
  * @return         The MPI rank which holds a quadrant at the given position for
  *                 the current p4est.

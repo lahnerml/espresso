6855df98498b55eb6aa9e5ed6dffc739caa54163
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2018-04-15 13:47:09 +0300

    Also apply "Improve neighbor search" to MINIMAL_GHOST

--- src/core/p4est_dd.cpp
+++ src/core/p4est_dd.cpp
@@ -446,7 +446,8 @@ void dd_p4est_init_internal_minimal (p4est_ghost_t *p4est_ghost, p4est_mesh_t *p
   num_local_cells = (size_t) ds::p4est->local_num_quadrants;
   num_ghost_cells = (size_t) p4est_ghost->ghosts.elem_count;
   num_cells = num_local_cells + num_ghost_cells;
-  
+  castable_unique_ptr<sc_array_t> ni = sc_array_new(sizeof(int));
+
   ds::p4est_shell.resize(num_cells);
 
   for (int i = 0; i < num_local_cells; ++i) {
@@ -477,8 +478,6 @@ void dd_p4est_init_internal_minimal (p4est_ghost_t *p4est_ghost, p4est_mesh_t *p
     
     for (int n = 0; n < 26; ++n) {
       ds::p4est_shell[i].neighbor[n] = -1;
-      sc_array_t *ni;
-      ni = sc_array_new(sizeof(int));
       p4est_mesh_get_neighbors(ds::p4est, p4est_ghost, p4est_mesh, i, n, NULL, NULL, ni);
       if (ni->elem_count > 1) // more than 1 neighbor in this direction
         printf("%i %i %li strange stuff\n",i,n,ni->elem_count);
@@ -489,7 +488,7 @@ void dd_p4est_init_internal_minimal (p4est_ghost_t *p4est_ghost, p4est_mesh_t *p
           ds::p4est_shell[i].shell = 1;
         }
       }
-      sc_array_destroy(ni);
+      sc_array_truncate(ni);
       
       if (ds::p4est_shell[i].neighbor[n] < 0 || ds::p4est_shell[i].neighbor[n] >= num_cells) {
         printf("Index OOB %i of %li (%li,%li)\n", ds::p4est_shell[i].neighbor[n], num_cells, num_local_cells, num_ghost_cells);
